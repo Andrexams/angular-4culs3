dba8f0c4955d163668d50a6f5552d4c51a0959b7
Andrexams <andrexams@gmail.com>
2020-04-05 20:22:13 -0300

    4 files modified, 3 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
+
 import {CartService} from '../cart.service';
 
 @Component({
@@ -10,14 +13,30 @@ export class CartComponent implements OnInit {
 
   items;
   total;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+   }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
     this.total = this.cartService.getTotalPrice();
+  }
+
+  onSubmit(customerData){
+    //process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+    console.warn('Your order has been submitted', customerData);
+    this.router.navigateByUrl('checkoutok');
   }
 
 }
---
+++ src/app/success-checkout/success-checkout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sucess-checkout',
+  templateUrl: './sucess-checkout.component.html',
+  styleUrls: ['./sucess-checkout.component.css']
+})
+export class SucessCheckoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
